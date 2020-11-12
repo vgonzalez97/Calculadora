9194725bf1ad9e73d6b3b85de769cfa083b6de88
Victor <vgonzalez@profile.es>
2020-11-12 16:13:06 +0100

    Swagger-ui añadido

--- src/main/java/com/profile/test4/Test4Application.java
+++ src/main/java/com/profile/test4/Test4Application.java
@@ -2,11 +2,13 @@ package com.profile.test4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * The Class Test4Application.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Test4Application {
 
 	/**
