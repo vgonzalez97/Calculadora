e63e4881eaae359e9dcb97396dbbdd1f5d47941f
Victor <vgonzalez@profile.es>
2020-11-12 14:52:54 +0100

    CalculadoraController ahora recibe los parametros por url con @ParthVariable

--- src/main/java/com/profile/test4/controller/CalculadoraController.java
+++ src/main/java/com/profile/test4/controller/CalculadoraController.java
@@ -2,11 +2,9 @@ package com.profile.test4.controller;
 
 import com.profile.test4.service.CalculadoraService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * The Class CalculadoraController.
@@ -19,13 +17,15 @@ public class CalculadoraController {
     @Autowired
     CalculadoraService calculadoraService;
 
-    @GetMapping(value = "/suma", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Integer Suma(@RequestParam Integer firstNumber, @RequestParam Integer secondNumber){
+    @GetMapping(value = "/suma/{firstNumber}/{secondNumber}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Integer Suma(@PathVariable(value = "firstNumber", required = true) Integer firstNumber, @PathVariable(value = "secondNumber", required = true) Integer secondNumber){
         return calculadoraService.suma(firstNumber,secondNumber);
     }
 
-    @GetMapping(value = "/resta", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Integer Resta(@RequestParam Integer firstNumber, @RequestParam Integer secondNumber){
+    @GetMapping(value = "/resta/{firstNumber}/{secondNumber}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Integer Resta(@PathVariable(value = "firstNumber", required = true) Integer firstNumber, @PathVariable(value = "secondNumber", required = true) Integer secondNumber){
         return calculadoraService.resta(firstNumber,secondNumber);
     }
 }
