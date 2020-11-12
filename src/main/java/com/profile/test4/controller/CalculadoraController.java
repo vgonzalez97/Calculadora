9209cbab0884c6062322b3164be4d53f8afd830d
Victor <vgonzalez@profile.es>
2020-11-12 14:31:04 +0100

    Añadido requestMapping a CalculadoraController

--- src/main/java/com/profile/test4/controller/CalculadoraController.java
+++ src/main/java/com/profile/test4/controller/CalculadoraController.java
@@ -4,10 +4,12 @@ import com.profile.test4.service.CalculadoraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculadora")
 public class CalculadoraController {
 
     @Autowired
